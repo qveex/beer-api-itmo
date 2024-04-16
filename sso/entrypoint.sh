00b554cc5bc3cfa5c12bdbe9c7e711ad6b06a3cd
Krutov777 <madrid211514@gmail.com>
2024-04-16 21:16:22 +0300

    change docker-compose.yml

--- sso/entrypoint.sh
+++ sso/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ожидаем, пока PostgreSQL станет полностью доступной
 echo "Ожидание доступности PostgreSQL..."
-until pg_isready -h db -p 5432 -U postgres; do
+until pg_isready -h auth-db -p 5432 -U postgres; do
 echo "PostgreSQL еще не готова..."
 sleep 1
 done
@@ -9,7 +9,7 @@ echo "PostgreSQL доступна."
 
 # Выполнение миграций
 echo "Запуск миграций..."
-go run ./cmd/migrator --db-url="postgresql://postgres:root@db:5432/auth?sslmode=disable" --migrations-path=./migrations
+go run ./cmd/migrator --db-url="postgresql://postgres:root@auth-db:5432/auth?sslmode=disable" --migrations-path=./migrations
 
 # Если миграции прошли успешно, запускаем приложение
 echo "Запуск приложения..."
